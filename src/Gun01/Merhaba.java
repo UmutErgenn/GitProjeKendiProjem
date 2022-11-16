793f2bf814ebaf020c3eda2b554c14c385012149
umut <umutergen6200@gmail.com>
2022-11-16 10:31:47 +0300

    For döngüsü düzenlendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -2,8 +2,8 @@ package Gun01;
 
 public class Merhaba {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dünya");
-        System.out.println("merhaba");
+        System.out.println("Merhaba dünya");
+
 
         for (int i = 0; i < 5; i++) {
             System.out.println("Tekrar Merhaba");
